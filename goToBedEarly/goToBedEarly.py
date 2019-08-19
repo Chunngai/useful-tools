aa3a1239636af5c249e8cce1900cf0c41472113f
Chunngai <mikumikku3939@icloud.com>
2019-08-19 14:20:10 +0800

    keyboard operation added. fixed some bugs.

--- goToBedEarly/goToBedEarly.py
+++ goToBedEarly/goToBedEarly.py
@@ -5,6 +5,7 @@ import tkinter as tk
 import tkinter.messagebox
 import datetime
 import time
+import math
 
 
 def check_time():
@@ -15,8 +16,7 @@ def check_time():
     go_to_bed_time = datetime.time(0, 0).strftime("%H%M")
 
     # compare
-    # if int(go_to_bed_time) <= int(current_time) <= int("0200"):
-    if go_to_bed_time == go_to_bed_time:
+    if int(go_to_bed_time) <= int(current_time) <= int("0200"):
         return True
     else:
         return False
@@ -63,32 +63,29 @@ def go_to_bed_early_win(count_):
         frame_2.pack(side=tk.RIGHT, padx=25)
 
         # select time after which the win will pop-up again
+        # make the window pop-up in 5 min
         frame_3 = tk.Frame(frame_2)
         frame_3.pack(side=tk.TOP, pady=3)
 
-        five_min = tk.Radiobutton(frame_3, text="05 min", variable=wait_time, value=5,
-                                  command=lambda: {five_min.select(), win.destroy()})
-        five_min.bind("<Return>", func=lambda event: {five_min.select(), win.destroy()})
+        five_min = tk.Radiobutton(frame_3, text="05 min", variable=wait_time, value=5, command=lambda: five_min.select())
+        five_min.bind("<Return>", func=lambda event: five_min.select())
         five_min.grid(row=0, column=0)
-        '''
-        five_min_button = tk.Button(frame_3, text="OK", command=win.quit)
-        five_min_button.bind("<Return>", lambda event: win.destroy())
-        five_min_button.grid(row=0, column=1)'''
 
+        # make the window pop-up in 10 min
         frame_4 = tk.Frame(frame_2)
         frame_4.pack(side=tk.BOTTOM, pady=3)
 
         ten_min = tk.Radiobutton(frame_4, text="10 min", variable=wait_time, value=10,
-                                 command=lambda: {ten_min.select(), win.destroy()})
-        ten_min.bind("<Return>", func=lambda event: {ten_min.select(), win.destroy()})
+                                 command=lambda: ten_min.select())
+        ten_min.bind("<Return>", func=lambda event: ten_min.select())
         ten_min.grid(row=0, column=0)
-        '''
-        ten_min_button = tk.Button(frame_4, text="OK", command=win.quit)
-        ten_min_button.bind("<Return>", lambda event: win.destroy())
-        ten_min_button.grid(row=0, column=1)'''
     else:
-        tk.Button(frame, text="OK", command=lambda: tk.messagebox.showwarning(title="SLEEP NOW!",
-                                                                              message="SLEEP NOW!")).pack()
+        button_warning = tk.Button(frame, text="OK", command=lambda: tk.messagebox.showwarning(title="SLEEP NOW!",
+                                                                                               message="SLEEP NOW!"))
+        button_warning.bind("<Return>", func=lambda event: tk.messagebox.showwarning(title="SLEEP NOW!",
+                                                                                     message="SLEEP NOW!"))
+        button_warning.focus()
+        button_warning.pack()
 
     # mainloop
     win.mainloop()
@@ -100,9 +97,8 @@ def go_to_bed_early_win(count_):
     # clicked "OK" only: 1 min
     wait_time_int = int(wait_time.get())
     if wait_time_int == 0:
-        wait_time_int = 1
+        wait_time_int = 0.83
 
-    print(wait_time_int)
     return wait_time_int
 
 
@@ -112,15 +108,18 @@ def go_to_bed_early():
         pass
 
     # pop-up the win
-    count = 0  # count times. make the win pop-up after 5 min or 10 min for 3 times at most.
+    count = 1  # count times. make the win pop-up after 5 min or 10 min for 3 times at most.
+    wait_time = 0  # 0.83 or 5 or 10
     while True:
-        count += 1
+        # count += 1 if "05 min" or "10 min" is selected
+        if wait_time == 5 or wait_time == 10:
+            count += 1
 
         # pop-up the win
         wait_time = go_to_bed_early_win(count)
 
         # by default wait for 1 min and pop-up again
-        time.sleep(wait_time * 60)
+        time.sleep(math.ceil(wait_time * 60))
 
 
 if __name__ == '__main__':
