d0b3229c34f293c2c22d89d1568e183071e80b72
Chunngai <mikumikku3939@icloud.com>
2019-08-20 10:58:17 +0800

    added func: write and load a schedule from the tmp.txt file

--- scheduleManaging/scheduleManaging.py
+++ scheduleManaging/scheduleManaging.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 
+import pyperclip
+
 from scheduleCreation import create_schedule
 from scheduleTimeModification import modify_time
 from scheduleTaskNameModification import modify_task_name
@@ -9,16 +11,32 @@ from scheduleTaskNameModification import modify_task_name
 
 def get_task():
     # choose a task
-    task_num = input("1. create a schedule\n2. modify a schedule\n3. modify a task name\nwhat to do? >>> ")
-    # input should be '1', '2' and '3'
-    while task_num not in ['1', '2', '3'] or not task_num.isdigit():
-        task_num = input("\rinput 1, 2 or 3 only! >>> ")
+    task_num = input(
+        """1. create a schedule
+        \r2. modify a schedule
+        \r3. modify a task name
+        \r4. copy schedule from txt
+        \rwhat to do? >>> """)
+    # input should be '1', '2', '3' and '4'
+    while task_num not in ['1', '2', '3', '4'] or not task_num.isdigit():
+        task_num = input("\rinput 1, 2, 3 or 4 only! >>> ")
     print()
 
     # str -> int
     task_num = int(task_num)
 
     return task_num
+
+
+def copy_from_txt():
+    with open("tmp.txt", encoding="utf-8") as f:
+        pyperclip.copy(f.read())
+
+
+def save_to_txt():
+    schedule = pyperclip.paste()
+    with open("tmp.txt", 'w', encoding="utf-8") as f:
+        f.write(schedule)
 
 
 def schedule_managing():
@@ -30,6 +48,10 @@ def schedule_managing():
         modify_time()
     if task == 3:
         modify_task_name()
+    if task == 4:
+        copy_from_txt()
+
+    save_to_txt()
 
 
 if __name__ == '__main__':
