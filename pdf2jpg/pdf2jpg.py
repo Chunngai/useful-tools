867ea0ca17436a9e2c5dfef7fa2c8fdb42a95ff2
Chunngai <mikumikku3939@icloud.com>
2019-09-06 01:00:21 +0800

    the original pdf2jpg -> ml_pdf2jpg and wrote a new pdf2jpg program for more general purposes

--- pdf2jpg/pdf2jpg.py
+++ pdf2jpg/pdf2jpg.py
@@ -6,38 +6,14 @@ import os
 from pdf2image import convert_from_path
 
 
-def pdf_exists(dir_path):
-    for file in os.listdir(dir_path):
-        if os.path.splitext(file)[1] == ".pdf":
-            return True
-
-    return False
-
-
 def pdf_2_image():
-    # get the path of the dir containing the pdf file
-    dir_path = input("input the path of the dir containing the pdf file >>> ")
-    while True:
-        if not os.path.exists(dir_path):
-            dir_path = input("the path does not exist! input again >>> ")
-        elif not pdf_exists(dir_path):
-            dir_path = input("there is no pdf file in the dir! input again >>> ")
-        else:
-            break
-
     # get the path of the pdf file
-    pdf_file_name = dir_path.split('\\')[-1]
-    pdf_path = os.path.join(dir_path, pdf_file_name + ".pdf")
-
-    # generate a dir for storing the images
-    image_dir = os.path.join(dir_path, pdf_file_name + "PdfImages")
-    try:
-        os.mkdir(image_dir)
-    except FileExistsError:
-        pass
+    pdf_path = input("input the path of the pdf file >>> ")
+    while not os.path.exists(pdf_path):
+        pdf_path = input("invalid path! input again >>> ")
 
     # pdf2image
-    convert_from_path(pdf_path, output_folder=image_dir, fmt="jpg")
+    convert_from_path(pdf_path, output_folder=os.getcwd(), fmt="jpg")
 
 
 if __name__ == '__main__':
