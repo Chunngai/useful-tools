52e9ba5109b1e7591870bfa8f62f6390f76ba94a
Chunngai <mikumikku3939@icloud.com>
2019-12-08 22:05:00 +0800

    question_index != 0 -> 0

--- questionBase/reorganize.py
+++ questionBase/reorganize.py
@@ -7,30 +7,27 @@ import pandas as pd
 def reorganize(file_name):
     # load the question base
     data = pd.read_csv(file_name + ".csv")
-    # print(len(data))
 
     # float -> str for the uncertain attr
     data["uncertain"] = data["uncertain"].astype(str)
     data.loc[data.uncertain == "nan", "uncertain"] = ""
 
+    data.loc[data.question_index != 0, "question_index"] = 0
+
     # incorrect
     incorrect_rows = data[~data["incorrect"].isin([0])]
-    # print(len(incorrect_rows))
 
     # uncertain
     correct_rows = data[data["incorrect"].isin([0])]
-    # print(len(correct_rows))
     correct_n_uncertain_rows = correct_rows[~correct_rows["uncertain"].isin([""])]
-    # print(len(correct_n_uncertain_rows))
 
     # correct_and_certain
     correct_n_certain_rows = correct_rows[correct_rows["uncertain"].isin([""])]
-    # print(len(correct_n_certain_rows))
 
+    # concatenation
     reorganized_data = pd.concat([incorrect_rows, correct_n_uncertain_rows, correct_n_certain_rows])
 
-    # print(len(reorganized_data))
-
+    # save the file
     reorganized_data.to_csv(file_name + " (reorganized).csv", index=False)
 
 
